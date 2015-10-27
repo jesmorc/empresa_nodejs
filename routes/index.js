381607edaded8e882a89a701fcab8340bf6d7821
jesmorc <jesusgg90@hotmail.com>
2015-10-28 00:21:01 +0100

    modificacion

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,36 @@
 
+
 /*
  * GET home page.
  */
 
+
+var empresas = [
+  { 
+    name: 'Futurum', 
+    comment: 'La empresa del futuro'
+  },
+  {
+    name: 'Fidesol',
+    comment: 'Situada en Granada'
+  },
+  {
+    name: 'Novasoft',
+    comment: 'Situada en Mengibar'
+  }
+];
+
+
+var assert = require("assert");
+
 exports.index = function(req, res){
-  res.render('index', { title: 'Puntuaciones de empresas de prácticas' });
+  var names = empresas.map(function(p) { return p.name; });
+  res.render('index', { empresas: names });
+  if (names.length < 4){
+	console.log("Hay menos de 4 empresas");
+	assert(names, "Minimo 4 empresas");
+  }
 };
+
+
+	
